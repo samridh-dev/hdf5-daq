8123591b039923e427d8b733759ee76718b48121
samridh-dev <singhsam@grinnell.edu>
2024-03-13 12:02:39 +0100

    updated cmake

--- cmake/config.cmake
+++ cmake/config.cmake
@@ -1,2 +1,4 @@
 option(ENABLE_BUILD_EXAMPLE "build the example binary"                    ON )
 option(ENABLE_BUILD_LIBRARY "build the library"                           ON )
+option(ENABLE_BUILD_TEST    "build the tests"                             OFF )
+option(ENABLE_BINARY_FOLDER "Dump all binaries to bin/"                   ON )
